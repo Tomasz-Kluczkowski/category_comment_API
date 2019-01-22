5d267d50e593098b97038ebd8ab67b3b99990921
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2019-01-22 13:17:47 +0000

    add comment resource in app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Blueprint
 from flask_restful import Api
 from resources.hello import Hello
 from resources.category import CategoryResource
+from resources.comment import CommentResource
+
 api_bp = Blueprint('api', __name__)  # create a master route /api
 api = Api(api_bp)  # create an instance of that /api
 
 # Adds a new route
 api.add_resource(Hello, '/hello')  # Hello is similar to a class based view in Django, add child route /hello to /api
 api.add_resource(CategoryResource, '/category')
+api.add_resource(CommentResource, '/comment')
