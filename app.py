2c1f31d61eaae46f271df2d0ddd42af2e7b9f4ee
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2019-01-22 10:49:24 +0000

    add model and marshmallow representations for output in the api.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.hello import Hello
-api_bp = Blueprint('api', __name__)
-api = Api(api_bp)
+api_bp = Blueprint('api', __name__)  # create a master route /api
+api = Api(api_bp)  # create an instance of that /api
 
 # Adds a new route
-api.add_resource(Hello, '/hello')
+api.add_resource(Hello, '/hello')  # Hello is similar to a class based view in Django, add child route /hello to /api
--- run.py
+++ run.py
@@ -5,4 +5,15 @@ def create_app(config_filename):
     app = Flask(__name__)
     app.config.from_object(config_filename)
 
-    from app import api
+    from app import api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')  # a blueprint is like a master route
+
+    from model import db
+    db.init_app(app)
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app('config')
+    app.run(debug=True)
