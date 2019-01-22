1576abda944c8164841e70e49f8043a4fbfca742
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2019-01-22 11:34:52 +0000

    add category resource in app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.hello import Hello
+from resources.category import CategoryResource
 api_bp = Blueprint('api', __name__)  # create a master route /api
 api = Api(api_bp)  # create an instance of that /api
 
 # Adds a new route
 api.add_resource(Hello, '/hello')  # Hello is similar to a class based view in Django, add child route /hello to /api
+api.add_resource(CategoryResource, '/category')
